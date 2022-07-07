fb4e70171395c7f7478e41ea60f9e9465a6304d0
Bunyood <bunyodbunyod035@gmail.com>
2022-07-07 22:07:25 +0500

    add header

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var elbtn = document.querySelector(".header-inner__btn-search") 
+var elsearch = document.querySelector(".header-inner__input-search")
+function addsearch() {
+
+
+elsearch.classList.toggle("header-inner__input-search--active")
+}
