e30ee98ce13638c1f6a0664ca91cb578eba82e17
Bunyood <bunyodbunyod035@gmail.com>
2022-07-07 22:36:37 +0500

    add hero carusel

--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@ function addsearch() {
 
 elsearch.classList.toggle("header-inner__input-search--active")
 }
+
+//! ////////////////////////////////////////////////////////////////////////////
+
+var elItem = document.querySelectorAll(".banner-right__list-item");
+var elLink = document.querySelectorAll(".banner-right__list-link");
+var elSlider = document.querySelectorAll(".banner-right__carusel");
+
+elLink.forEach(function (links) {
+  links.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    elItem.forEach(function (item) {
+      item.classList.remove("banner-right__list-item--active")
+    });
+    links.parentElement.classList.add("banner-right__list-item--active");
+
+    elSlider.forEach(function (item) {
+      item.classList.remove("banner-right__carusel--active")
+    });
+    document.querySelector(links.getAttribute('href')).classList.add("banner-right__carusel--active")
+    
+  })
+  })
+  
